d95030e455f1da6a051992b3f2ba1d8cae8583dc
Radosław Gryta <radek.gryta@gmail.com>
2025-03-06 10:39:23 +0100

    Add default token_type

--- api-client/src/commonMain/kotlin/io/wellmate/api/client/dataclasses/auth/Token.kt
+++ api-client/src/commonMain/kotlin/io/wellmate/api/client/dataclasses/auth/Token.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Token(
     @SerialName("access_token") val accessToken: String,
-    @SerialName("token_type") val tokenType: String
+    @SerialName("token_type") val tokenType: String = "Bearer",
 ) {
     val authorizationHeader: String
         get() = "$tokenType $accessToken"
